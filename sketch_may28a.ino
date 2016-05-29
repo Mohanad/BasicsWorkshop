e7d4d0093f8c0d03a836031f12a093e7ca70a779
Mohanad Ahmed <mohanadahmed@windowslive.com>
2016-05-29 17:45:49 +0300

    Street Light Example

---
+++ sketch_may28a.ino
@@ -0,0 +1,19 @@
+#include <SoftwareSerial.h>
+//SoftwareSerial serial(2,3);
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+}
+int val;
+void loop() {
+  // put your main code here, to run repeatedly:
+  //Serial.write("test\r\n");
+  val = analogRead(0);
+  Serial.println(val);
+  if(val > 500){
+    digitalWrite(13, LOW);
+  }else{
+    digitalWrite(13, HIGH);
+  }
+}
