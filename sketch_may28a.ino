c7a15d7ec145201d141b67d7eebf887396c62dde
Mohanad Ahmed <mohanadahmed@windowslive.com>
2016-05-29 18:02:23 +0300

    Push Button Program

--- sketch_may28a.ino
+++ sketch_may28a.ino
@@ -1,19 +1,15 @@
 #include <SoftwareSerial.h>
-//SoftwareSerial serial(2,3);
+// Program to Check Push button functionality.
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+  pinMode(2, INPUT_PULLUP);
   pinMode(13, OUTPUT);
 }
 int val;
 void loop() {
   // put your main code here, to run repeatedly:
-  //Serial.write("test\r\n");
-  val = analogRead(0);
-  Serial.println(val);
-  if(val > 500){
-    digitalWrite(13, LOW);
-  }else{
+  if(digitalRead(2)){
     digitalWrite(13, HIGH);
+  }else{
+    digitalWrite(13, LOW);
   }
 }
